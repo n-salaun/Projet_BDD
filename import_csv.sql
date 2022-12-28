651c53d26ef5579bfde343747c3dc9a89805065f
n-salaun <107122757+n-salaun@users.noreply.github.com>
2022-12-28 15:05:13 +0100

    Delete import_csv.sql

--- import_csv.sql
+++
@@ -1,3 +0,0 @@
-  SET datestyle = dmy;
-  \copy clients FROM '/home/nsalaun/Cours/Projet_BDD/CSV/clients.csv' WITH (FORMAT csv, HEADER true);
-  \copy adresse FROM '/home/nsalaun/Cours/Projet_BDD/CSV/adresses.csv' WITH (FORMAT csv, HEADER true);
